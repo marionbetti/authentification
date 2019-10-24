6bcfec4de4845e91bc687ec5432861a70db5fcab
marion <marionpicard@yahoo.fr>
2019-10-24 15:02:38 +0200

    ne plus affichez mot de passe

--- router/utilisateurs.js
+++ router/utilisateurs.js
@@ -2,27 +2,31 @@ const { validation, Utilisateur } = require("../model/utilisateurs");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const _ = require("lodash");
 
 router.get("/", (req, resp) => {
   Utilisateur.find().then(result => {
-    resp.send(result);
+    const resultModif = [];
+    for (let i = 0; i < result.length; i++) {
+      resultModif[i] = _.pick(result[i], ["id", "login", "role"]);
+    }
+    resp.send(resultModif);
   });
 });
-
 // Postman
 // http://localhost:5000/api/utilisateurs
 // POST
 // { "login" : "test@yahoo.fr" , "mdp" : "azerty" , "role" : "admin"}
 router.post("/", (req, resp) => {
+  // 1
   validation(req.body)
     .then(() => {
+      // 2 si ok => je passe à l'étape suivante
       // vérifier que les données transmises sont conformes
-
-      // si ok => je passe à l'étape suivante
 
       // vérifier que le login (email) n'existe pas déjà ?
       Utilisateur.find({ login: req.body.login }).then(result => {
-        //console.log(result);
+        // 3 soit j'ai ...
         if (result.length !== 0)
           return resp.status(400).send("login existe déjà !");
         // si ok => je passe à l'étape suivante
@@ -30,24 +34,27 @@ router.post("/", (req, resp) => {
 
         //Crypter le mot de passe
         // ralentir le hacker pour le vol d'identité
-
+        // 5 req.body.mdp que je salt => hashedPassword
         bcrypt.genSalt(10).then(salt => {
           bcrypt.hash(req.body.mdp, salt).then(hashedPassword => {
-            //créer le profil avec un mot de passe hashé !
+            // 6 créer le profil avec un mot de passe hashé !
             const utilisateur = new Utilisateur({
               login: req.body.login,
               mdp: hashedPassword,
               role: req.body.role
             });
-
-            utilisateur.save().then(() => {
-              resp.send("nouvel utilisateur créé !");
+            // 7 create mon profil
+            utilisateur.save().then(nouveauCompte => {
+              // 8 retourner le nouveau compte avec {"_id": "foiiohohlb"}
+              const result = _.pick(nouveauCompte, ["_id", "role"]);
+              resp.send(result);
             });
           });
         });
       });
     })
     .catch(error => {
+      // 2 si ok
       resp.status(400).send(error.details[0].message);
     });
 });
