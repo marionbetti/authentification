27c7f65b6b21a37049be89593cb5c7a0aec9e84e
marion <marionpicard@yahoo.fr>
2019-10-24 15:30:16 +0200

    création du router connexion

---
+++ router/connexion.js
@@ -0,0 +1,14 @@
+// ici c'est ce fichiers qui est appeller lorsque l'on est
+// devant une page qui demande  un login /mot de passe
+
+// route a part pour les connexions
+
+// vérifier que les informations sont conformes
+
+// si pas conformes => retour c'est pas conforme
+// si conforme => jwt
+
+const express = require("expres");
+const router = express.Router();
+
+module.exports = router;
